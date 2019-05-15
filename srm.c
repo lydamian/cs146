0de841fa97af8457b9427531b773b0e9afa8bba6
Damian Sr. Ly <dsly@andromeda-74.ics.uci.edu>
2019-05-14 20:27:30 -0700

    IMplemented unrm and srm, unrm has a bug where directories arent being unrm'd to the current directory but is throwing an error

--- srm.c
+++ srm.c
@@ -8,29 +8,38 @@ void pl();
 char *get_trash();
 int are_options(char *argv[]);
 int is_option(char *arg); 
+int safe_rm(char *file);
+int regular_rm(char *file);
 
-int main(){
+// GLOBALS
+char *TRASH;
+
+int main(int argc, char *argv[]){
 	printf("Hello from srm.c\n");
 	//local variables
-	char *trash;
 	struct dirent *de;
 	DIR *dir;
-	int is_option;
+	int option; // boolean value
 
 	// check  if trash variable defined
-	if((trash = get_trash()) == NULL){
+	if((TRASH = get_trash()) == NULL){
 		printf("Error, TRASH environment variable not defined\n");
 	}
 
 	// check if trash is a valid directory
-	if((dir = opendir(trash)) == NULL){
+	if((dir = opendir(TRASH)) == NULL){
 		printf("ERROR, invalid TRASH environment variable\n");
 	}
 
-	// check if there are options
-
+	for(int i = 1; argv[i] != NULL; i++){
+		if(!is_option(argv[i])){
+			if(safe_rm(argv[i]) < 0){
+				printf("Error, safe removing file: %s\n", argv[i]);
+				exit(1);
+			}
+		}
+	}
 	
-
 	// closing the directory
 	closedir(dir);
 
@@ -45,12 +54,41 @@ char *get_trash(){
 	return getenv("TRASH");
 }
 
+
+/* NOTE
+if you use "global trash directory", then
+   remember that "srm" must work even if you're srm'ing a file that is
+      not on the same mounted file 
+*/
+int safe_rm(char *file){
+	printf("safe removing %s\n", file);
+	char *newpath = strcat(get_trash(), "/");
+	newpath = strcat(newpath, file);
+	printf("the new path is: %s\n", newpath);
+	if(rename(file, newpath) < 0){
+		return -1;
+	}
+	return 0;
+}
+
+int regular_rm(char *file){
+	printf("regular removing %s\n", file);
+}
+
 // checks if at least one option exists 
 int are_options(char *argv[]){
-
+	for(int i = 1; argv[i] != NULL; i++){
+		if(is_option(argv[i])){
+			return 1;
+		}
+	}
+	return 0;
 }
 
 // checks if the given char array is an option
 int is_option(char *arg){
-
+	if(arg[0] == '-'){
+		return 1;
+	}
+	return 0;
 }
--- unrm.c
+++ unrm.c
@@ -2,7 +2,112 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <dirent.h>
+/*
+ *	Description: unremove all the files/directories in the trash
+ *	and put them into the current directory
+ * 
+ */
+
+void pl();
+char *get_trash();
+int are_options(char *argv[]);
+int is_option(char *arg); 
+int safe_rm(char *file);
+int regular_rm(char *file);
+int unrm(char *curr_dir);
+int naming_collision(char *filename);
+
+// GLOBALS
+char *TRASH;
 
 int main(int argc, char *argv[]){
 	printf("Hello from unrm.c\n");
+	//local variables
+	DIR *dir;
+
+	// check  if trash variable defined
+	if((TRASH = get_trash()) == NULL){
+		printf("Error, TRASH environment variable not defined\n");
+	}
+
+	// check if trash is a valid directory
+	if((dir = opendir(TRASH)) == NULL){
+		printf("ERROR, invalid TRASH environment variable\n");
+	}
+
+	// mv all the files in the trash to the current directory
+	// handle the case of overwrriting someone else's files
+	unrm(".");
+	
+	// closing the directory
+	closedir(dir);
+
+	return 0;
+}
+
+void pl(){
+	printf("\n");
+}
+
+char *get_trash(){
+	return getenv("TRASH");
+}
+
+// checks if the given char array is an option
+int is_option(char *arg){
+	if(arg[0] == '-'){
+		return 1;
+	}
+	return 0;
+}
+
+int unrm(char *curr_path){
+	DIR *dir;
+	struct dirent *p;;
+	char response;
+	char *temp = strcat(TRASH, "/");
+
+	// opening directory
+	dir = opendir(TRASH);
+	if(!dir){
+		printf("error, opening directory\n");
+		exit(1);
+	}
+
+	// iterating through all files int the directory
+	while((p = readdir(dir)) != NULL){
+		if((strcmp(p->d_name, ".") != 0) && (strcmp(p->d_name, "..") != 0)){
+			char *oldpath = strcat(temp, p->d_name);		
+			if(naming_collision(p->d_name)){
+				printf("File '%s' already exists in current directory overwrite? y/n\n", p->d_name);
+				scanf("%c", &response);
+				if(response == 'y'){
+					printf("overwritting\n");		
+					if(rename(oldpath, p->d_name) < 0){
+						printf("Error unremoving\n");
+						return -1;
+					}
+				}
+			}
+			else{
+				printf("there is no naming collision\n");
+				printf("%s \n", p->d_name);
+				if(rename(oldpath, p->d_name) < 0){
+					printf("error removing\n");
+					return -1;
+				}
+			}
+		}
+	}
+
+	closedir(dir);
+	return 1;
+}
+
+int naming_collision(char *filename){
+	if(access(filename, F_OK) == 0){
+		return 1;
+	}
+	return 0;
 }
